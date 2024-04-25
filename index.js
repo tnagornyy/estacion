def0669c3a679acbe608dc93865faa9c855c4408
tnagornyy <164346438+tnagornyy@users.noreply.github.com>
2024-04-25 13:56:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let countEl = document.getElementById("count-el");
+console.log(countEl)
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment(){
+   
+    count = count + 1
+   
+    countEl.textContent = count    
+}
+
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
